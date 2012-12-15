1d27c7bca23b95ee80f46b90d6682bd54d98ce91
Ankit Sardesai <amsardesai@gmail.com>
2012-12-15 01:07:34 -0500

    worked more on JS and CSS

--- main.js
+++ main.js
@@ -5,11 +5,33 @@ function multM(a,b) {
 		for (j=0;j<3;j++)
 			for (k=0;k<3;k++)
 				m[i][j] += a[i][k]*b[k][j];
-
 	return m;
 }
 
+function translate(n,x,y) {
+	var m = [[0,0,0],[0,0,0],[0,0,0]];
+	for (i=0;i<3;i++) 
+		for (j=0;j<3;j++)
+			m[i][j] = n[i][j];
+	m[0][2] += x;
+	m[1][2] += y;
+	return m;
+}
 
+function scale(n,f) {
+	m = [[f,0,0],[0,f,0],[0,0,1]];
+	return multM(n,m);
+}
+
+function rotate(n,d) {
+	r = d*(Math.PI/180);
+	m = [[Math.cos(r),-Math.sin(r),0],[Math.sin(r),Math.cos(r),0],[0,0,1]];
+	return multM(n,m);
+}
+
+function skew() {
+	
+}
 
 
 // Adding transformations
