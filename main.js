3d06a1cfb319af48f891ae416c42341a464fe202
Ankit Sardesai <amsardesai@gmail.com>
2012-12-15 20:11:07 -0500

    Split jquery functions into new file and added more calculation functions

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-var trans[]; // Global transformation functions
+// -----------------------------------
+// Variable Declarations
+
+var trans[]; // Global transformation array
 var I = [[1,0,0],[0,1,0],[0,0,1]]; // Identity matrix
+var curTrans = [[0,0,0],[0,0,0],[0,0,0]]; // Current transformation
 
 // -----------------------------------
 // Matrix Modification
@@ -55,18 +59,39 @@ function Transformation(t,arg1,arg2) {
 	this.arg2 = arg2;
 }
 
+function calculateTransformation() {
+	curTrans = [[0,0,0],[0,0,0],[0,0,0]];
+	for (i=0;i<3;i++) 
+		for (j=0;j<3;j++)
+			curTrans[i][j] = I[i][j];
+
+	for (i=0;i<trans.length;i++) {
+		switch(trans[i].trans) {
+			case 0: // Translate
+				curTrans = translate(curTrans,trans[i].arg1,trans[i].arg2);
+			break;
+			case 1: // Scale
+				curTrans = scale(curTrans,trans[i].arg1,trans[i].arg2);
+			break;
+			case 2: // Rotate
+				curTrans = rotate(curTrans,trans[i].arg1);
+			break;
+			case 3: // Skew
+				curTrans = skew(curTrans,trans[i].arg1,trans[i].arg2);
+			break;
+		}
+	}
+}
+
 function addTransformation(t,arg1,arg2) {
 	m = new Transformation(t,arg1,arg2);
 	trans.push(m);
+	calculateTransformation();
 }
 
 function removeTransformation(n) {
-
-}
-
-function calculateTransformation() {
-
-
+	trans.splice(n,1);
+	calculateTransformation();
 }
 
 // -----------------------------------
@@ -89,27 +114,3 @@ function alertM(m) {
 
 // -----------------------------------
 // jQuery Document Ready
-
-$(document).ready(function() {
-	trans = new Array();
-
-
-	
-
-	$("#wrapper #toolslist .transformation").click(function() {
-		switch ($(this).attr("id")) {
-			case "translate":
-
-			break;
-			case "scale":
-
-			break;
-			case "rotate":
-
-			break;
-			case "skew":
-
-			break;
-		}
-	});	
-})
