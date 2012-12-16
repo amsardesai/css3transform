71ad1f034d51d18c69aceaac24322eb22590e626
Ankit Sardesai <amsardesai@gmail.com>
2012-12-15 22:20:19 -0500

    Removed unneeded extra file

--- mainfunctions.js
+++ mainfunctions.js
@@ -98,16 +98,16 @@ function removeTransformation(n) {
 
 // -----------------------------------
 // Porting to View
-/*
+
 function addToPage(transformation) {
 
 
 }
 
 function displayTransformation() {
-	//obj = $("#wrapper #workpanel #object");
+	obj = $("#wrapper #workpanel #object");
 }
-*/
+
 function alertM(m) {
 	s="";
 	var i,j;
@@ -116,3 +116,35 @@ function alertM(m) {
 			s+= " " + m[i][j];
     alert(s);
 }
+
+// -----------------------------------
+// jQuery Document Ready
+
+$(document).ready(function() {
+	trans = new Array();
+
+	$("#wrapper #toolslist .transformation").click(function() {
+		switch ($(this).attr("id")) {
+			case "translate":
+				var tx = 10;
+				var ty = 10;
+				addTransformation(0,tx,ty);
+			break;
+			case "scale":
+				var fx = 2;
+				var fy = 2;
+				addTransformation(1,fx,fy);
+			break;
+			case "rotate":
+				var d = 45;
+				addTransformation(2,d,0);
+			break;
+			case "skew":
+				var dx = 30;
+				var dy = 25;
+				addTransformation(3,dx,dy);
+			break;
+		}
+	});	
+});
+
