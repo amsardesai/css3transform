bf6de7c00dbc32f28fcd0536806e425c0e3c2334
Ankit Sardesai <amsardesai@gmail.com>
2012-12-16 21:11:08 -0500

    adding transformations into list now works

--- mainfunctions.js
+++ mainfunctions.js
@@ -94,11 +94,13 @@ function addTransformation(t,arg1,arg2) {
 	};
 	trans.push(m);
 	calculateTransformation();
+	refreshList();
 }
 
 function removeTransformation(n) {
 	trans.splice(n,1);
 	calculateTransformation();
+	refreshList();
 }
 
 function modifyTransformation(n,t,arg1,arg2) {
@@ -111,10 +113,65 @@ function modifyTransformation(n,t,arg1,arg2) {
 // Porting to View
 
 function refreshList() {
+/*
+		<div class="transformation tTranslate">
+			<a class="button" href="#"></a>
+			<span>Translate</span>
+			<div class="input">
+				x-axis: <input type="text" name="xcoords"> pixels<br />
+				y-axis: <input type="text" name="ycoords"> pixels
+			</div>
+		</div>
+		<div class="transformation tScale">
+			<a class="button" href="#"></a>
+			<span>Scale</span>
+			<div class="input">
+				factor: <input type="text" name="factor"> times
+			</div>
+		</div>
+		<div class="transformation tRotate">
+			<a class="button" href="#"></a>
+			<span>Rotate</span>
+			<div class="input">
+				angle: <input type="text" name="angle"> degrees
+			</div>
+		</div>
+		<div class="transformation tSkew">
+			<a class="button" href="#"></a>
+			<span>Skew</span>
+			<div class="input">
+				x-axis: <input type="text" name="xcoords"> degrees<br />
+				y-axis: <input type="text" name="ycoords"> degrees
+			</div>
+		</div>
+	*/
+
+	var container = $("#tools #toolsused");
+	container.html(" ");
+	var i;
+	for (i=0;i<trans.length;i++) {
+		var cur = trans[i];
+		var curObj = $("<div>").addClass("transformation").attr("name",i+1);
+		curObj.append($("<a>").addClass("button").attr("href","#"));
+		if (cur.t==0) {
+			curObj.addClass("tTranslate")
+				.append($("<span>").html("Translate"));
+		} else if (cur.t==1) { 
+			curObj.addClass("tScale")
+				.append($("<span>").html("Scale"));
+		} else if (cur.t==2) {
+			curObj.addClass("tRotate")
+				.append($("<span>").html("Rotate"));
+		} else if (cur.t==3) { 
+			curObj.addClass("tSkew")
+				.append($("<span>").html("Skew"));
+		}
+		container.append(curObj);
+	}
 
 
 
-
+	$("#tools .transformation input").attr("maxlength",6);
 }
 
 function displayTransformation() {
@@ -133,8 +190,8 @@ function displayTransformation() {
 
 	var mat = "matrix("+e[0][0]+","+e[1][0]+","+e[0][1]+","+e[1][1]+","+e[0][2]+","+e[1][2]+")";
 	obj.css("transform",mat);
-	obj.css("top",wHeight/2-oHeight/2);
-	obj.css("left",wWidth/2-oWidth/2);
+	obj.css("top",($("#workpanel").height()-120)/2-(obj.height())/2);
+	obj.css("left",($("#workpanel").width()-350)/2-(obj.width())/2);
 }
 
 function alertM(m) {
@@ -150,44 +207,41 @@ function alertM(m) {
 // jQuery Document Ready
 
 $(document).ready(function() {
-	$("#tools .transformation").attr('unselectable','on').css('user-select','none').on('selectstart',false);
+	$("#tools #toolslist .transformation").attr('unselectable','on').css('user-select','none').on('selectstart',false);
 	displayTransformation();
 
-	$("#tools #toolslist .transformation").click(function() {
-		$();
-		switch ($(this).attr("id")) {
+	// Controller for adding
+	$("#tools #toolslist .transformation .button").click(function() {
+		switch ($(this).parent().attr("id")) {
 			case "translate":
-				var tx = 10;
-				var ty = 10;
+				var tx = 0;
+				var ty = 0;
 				addTransformation(0,tx,ty);
 			break;
 			case "scale":
-				var fx = 2;
-				var fy = 2;
-				addTransformation(1,fx,fy);
+				var f = 1.0;
+				addTransformation(1,f,f);
 			break;
 			case "rotate":
-				var d = 45;
+				var d = 0;
 				addTransformation(2,d,0);
 			break;
 			case "skew":
-				var dx = 30;
-				var dy = 25;
+				var dx = 0;
+				var dy = 0;
 				addTransformation(3,dx,dy);
 			break;
 		}
+	});
 
-	});	
+	// Controller for removing
+	$("#tools #toolsused .transformation .button").click(function() {
+		alert("hi");
+		var id = $(this).parent().attr("name");
+		removeTransformation(id);
+	});
 
 	$(window).resize(displayTransformation);
-	$("#tools .transformation input").attr("maxlength",6);
-
-/*	$("#tools .transformation input").autoGrowInput({
-		maxWidth: 60,
-		minWidth: 0,
-		comfortZone: 10
-	});
-*/
 
 });
 
